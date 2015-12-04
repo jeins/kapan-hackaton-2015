f09f49b618771cb53e92704056a5dfcaef74a49f
hilmanrdn <hilmanhgb@gmail.com>
2015-12-04 10:52:14 +0100

    add materialize css

---
+++ Frontend-materialize/js/bangunController.js
@@ -0,0 +1,178 @@
+//---------------------------------------------------------------------------------------------------
+//--------------------- bangunApp Main Controller ---------------------------------------------------
+//---------------------------------------------------------------------------------------------------
+bangunApp.controller('mainCtrl', function($scope, bangunService, $http, $location){
+
+  //--------------- get all projects ----------------------------------
+  bangunService.getProjects(function(response){
+        $scope.projects = response.data;
+  });
+
+  //--------------- leaflet Map @Home ----------------------------------
+  $scope.showPeta = function() {
+   $('#kartu_proyek').fadeOut();
+   $('#map').fadeIn();
+ }
+
+  $scope.showKartu = function() {
+   $('#map').fadeOut();
+   $('#kartu_proyek').fadeIn();
+  }
+
+});
+
+
+//---------------------------------------------------------------------------------------------------
+//--------------------- bangunApp Prof Controller ---------------------------------------------------
+//---------------------------------------------------------------------------------------------------
+bangunApp.controller('profilCtrl', function($scope, bangunService, $http, $location){
+
+    //--------------- get Profil ----------------------------------------
+    bangunService.getProfile(function(response){
+        $scope.profil_infos = response.data;
+    });
+
+    //--------------- Info Proyek per Pemerintah -----------------------
+    bangunService.getProjectEach(function(response){
+          var hash_pemId = $location.hash();
+          var totalProyek = 0;
+          var data = response.data;
+          var arrayProject = [];
+
+          for(var i=0; i<Object.keys(data).length; i++){
+              if(hash_pemId == data[i].profile_pemerintah_id){
+                arrayProject[totalProyek] = data[i];
+                totalProyek ++;
+              }
+          }
+
+          $scope.projectsEach = arrayProject;
+          $scope.profil_infos.totalProyek = totalProyek;
+    });
+
+});
+
+
+//---------------------------------------------------------------------------------------------------
+//--------------------- bangunApp Halaman Detail Controller ------------------------------------------
+//---------------------------------------------------------------------------------------------------
+bangunApp.controller('detailCtrl', function($scope, bangunService, $http, $location){
+
+  //--------------- get project detail ----------------------------------
+  bangunService.getProjectDetail(function(response){
+
+      var data = response.data;
+      $scope.project = data;
+
+      //get info pemilik proyek untuk di halaman detail.html
+      $http.get(_URL + '/api/profile/' + data.profile_pemerintah_id)
+      .success(function(dataP) {
+           $('#jabatan_pemilik').text(dataP.fullname);
+      });
+  });
+
+});
+
+//---------------------------------------------------------------------------------------------------
+//--------------------- Login Dashboard Controller -------------------------------------------------
+//---------------------------------------------------------------------------------------------------
+bangunApp.controller('dashboardCtrl', function($scope, bangunService, $http, $location){
+
+  //--------------- login Pemerintah ----------------------------------
+  $scope.loginSubmit = function(){
+      // console.log(this.username);
+      var inputData = {
+        email: this.email,
+        password: this.password
+      };
+
+      $http.post(_URL + '/auth/admin/login', inputData)
+      .success(function(data) {
+        console.log(data.token);
+      })
+      .error(function(data){
+        console.log("cannot login!");
+      });
+  };
+
+  //--------------- register Pemerintah ----------------------------------
+  $scope.registerSubmit = function(){
+      // console.log(this.username);
+      var inputData = {
+        fullname: this.fullname,
+        jabatan: this.jabatan,
+        email: this.email,
+        password: this.password
+      };
+
+      $http.post(_URL + '/auth/admin/signup', inputData)
+      .success(function(data) {
+        console.log(data.token);
+      })
+      .error(function(data){
+        console.log("cannot signup!");
+      });
+  };
+
+});
+
+//---------------------------------------------------------------------------------------------------
+//--------------------- Submit proyek Controller -------------------------------------------------
+//---------------------------------------------------------------------------------------------------
+bangunApp.controller('proyekBaruCtrl', function($scope, bangunService, $http, $location){
+
+  //--------------- submit proyek baru ----------------------------------
+  $scope.submitProyek = function(){
+
+      var inputData = {
+        nama: this.judul_proyek,
+        profile_pemerintah_id: this.pem_id,
+        jenis: this.jenis_proyek,
+        deskripsi: this.desc_proyek,
+        file: this.file_proyek,
+        lokasi: this.lokasi,
+        status_selesai: this.status_proyek,
+        biaya: this.biaya,
+        outcome: this.outcome,
+        jadwal_terealisasi: this.targetWaktu_proyek
+      };
+
+      $http.post(_URL + '/admin/project', inputData)
+      .success(function(data) {
+        console.log("input proyek berhasil");
+      })
+      .error(function(data){
+        console.log("error input proyek");
+      });
+  };
+
+  //--------------- edit proyek lama ----------------------------------
+  $scope.submitEditProyek = function(){
+
+      var hash_proyek_id = $location.hash();
+
+      //belum ada progressbar
+      var inputData = {
+        id_proyek: hash_proyek_id,
+        nama: this.judul_proyek,
+        profile_pemerintah_id: this.pem_id,
+        jenis: this.jenis_proyek,
+        deskripsi: this.desc_proyek,
+        file: this.file_proyek,
+        lokasi: this.lokasi,
+        status_selesai: this.status_proyek,
+        biaya: this.biaya,
+        outcome: this.outcome,
+        jadwal_terealisasi: this.targetWaktu_proyek
+      };
+
+      $http.put(_URL + '/admin/project', inputData)
+      .success(function(data) {
+        console.log("edit proyek berhasil");
+      })
+      .error(function(data){
+        console.log("error edit proyek");
+      });
+  };
+
+});
---
+++ Frontend-materialize/js/custom-bangun.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  $('.button-collapse').sideNav();
+
+  //smoothscroll
+  $(".scroll_down").click(function() {
+       $('html, body').animate({
+           scrollTop: $("#proyek_wrapper").offset().top
+       }, 1000);
+   });
+
+  $('.modal-login-trigger').leanModal();
+  $('.modal-daftar-trigger').leanModal();
+  $('.modal-komentar-trigger').leanModal();
+
+  $('.datepicker').pickadate({
+  selectMonths: true, // Creates a dropdown to control month
+  selectYears: 15 // Creates a dropdown of 15 years to control year
+});
+
+
+}); // end of document ready
