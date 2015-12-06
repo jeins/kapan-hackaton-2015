bc87a15668fb3ebaa4ed33723c3fcf7ca16d4def
jeins <mail@mjuanakbar.info>
2015-12-06 09:40:14 +0100

    backend add projectprogressseeder

--- KapanBackend/database/seeds/ProjectProgressSeeder.php
+++ KapanBackend/database/seeds/ProjectProgressSeeder.php
@@ -12,26 +12,30 @@ class ProjectProgressSeeder extends Seeder
     public function run()
     {
         $progress = [
-        	[
-        		'project_info_id'	=> 1,
-        		'angka_progress' => 50,
-            'tanggal_update' => '0000-00-00 00:00:00'
-        	],
-        	[
-        		'project_info_id'	=> 2,
-            'angka_progress' => 25,
-            'tanggal_update' => '0000-00-00 00:00:00'
-        	],
-          [
-        		'project_info_id'	=> 3,
-            'angka_progress' => 75,
-            'tanggal_update' => '0000-00-00 00:00:00'
-        	],
-          [
-        		'project_info_id'	=> 4,
-            'angka_progress' => 0,
-            'tanggal_update' => '0000-00-00 00:00:00'
-        	]
+            [
+                'project_info_id'	=> 1,
+                'angka_progress' => 50,
+                'description' => 'asdas asdas asdasd ',
+                'tanggal_update' => '0000-00-00 00:00:00'
+            ],
+            [
+                'project_info_id'	=> 2,
+                'angka_progress' => 25,
+                'description' => 'asdas asdas asdasd ',
+                'tanggal_update' => '0000-00-00 00:00:00'
+            ],
+            [
+                'project_info_id'	=> 3,
+                'angka_progress' => 75,
+                'description' => 'asdas asdas asdasd ',
+                'tanggal_update' => '0000-00-00 00:00:00'
+            ],
+            [
+                'project_info_id'	=> 4,
+                'angka_progress' => 0,
+                'description' => 'asdas asdas asdasd ',
+                'tanggal_update' => '0000-00-00 00:00:00'
+            ]
         ];
 
         DB::table('project_progress')->insert($progress);
