549ff9eef40bcd4277ede74c0601b7075fcfe386
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2015-12-06 15:41:10 +0100

    rename projectinfo column + rakyat seeder
    projectinfo : status_selesai => status_project
    20 dummy profile rakyat

--- KapanBackend/database/seeds/InfoProjectSeeder.php
+++ KapanBackend/database/seeds/InfoProjectSeeder.php
@@ -19,7 +19,7 @@ class InfoProjectSeeder extends Seeder
                 'deskripsi'         => 'project ini adalah .....',
                 'outcome'           => 'project ini adalah .....',
                 'lokasi'            => '{long:123, lat:456}',
-                'status_selesai'    => 0,
+                'status_project'    => 0,
                 'biaya'             => '123.123.123.123',
                 'waktu_pelaksanaan' => '0000-00-00 00:00:00',
                 'jadwal_realisasi'  => '0000-00-00 00:00:00'
@@ -31,7 +31,7 @@ class InfoProjectSeeder extends Seeder
                 'deskripsi'         => 'project ini adalah .....',
                 'outcome'           => 'project ini adalah .....',
                 'lokasi'            => '{long:123, lat:456}',
-                'status_selesai'    => 0,
+                'status_project'    => 0,
                 'biaya'             => '1238.123.1512',
                 'waktu_pelaksanaan' => '0000-00-00 00:00:00',
                 'jadwal_realisasi'  => '0000-00-00 00:00:00'
--- KapanBackend/database/seeds/ProfileRakyatSeeder.php
+++ KapanBackend/database/seeds/ProfileRakyatSeeder.php
@@ -23,6 +23,96 @@ class ProfileRakyatSeeder extends Seeder
         		'email'		=> 'sumardi@gmail.com',
         		'password'	=> Hash::make('testhellow')
         	],
+          [
+        		'fullname'	=> 'Javier Sumanto',
+        		'email'		=> 'sumanto@gmail.com',
+        		'password'	=> Hash::make('akusumantohihi')
+        	],
+          [
+        		'fullname'	=> 'Michael Cecep',
+        		'email'		=> 'cecep@gmail.com',
+        		'password'	=> Hash::make('cecepgorbacep')
+        	],
+          [
+        		'fullname'	=> 'Ricki Putirai',
+        		'email'		=> 'ricky@gmail.com',
+        		'password'	=> Hash::make('akupenyerang')
+        	],
+          [
+        		'fullname'	=> 'Boaz Igbonefo',
+        		'email'		=> 'boaz@gmail.com',
+        		'password'	=> Hash::make('papuamerdeka')
+        	],
+          [
+        		'fullname'	=> 'Bambang Novanto',
+        		'email'		=> 'bambang@gmail.com',
+        		'password'	=> Hash::make('bamssamson')
+        	],
+          [
+        		'fullname'	=> 'Setya Pamungkas',
+        		'email'		=> 'setya@gmail.com',
+        		'password'	=> Hash::make('akulelakisetia')
+        	],
+          [
+        		'fullname'	=> 'Jan Gayus',
+        		'email'		=> 'gayus@gmail.com',
+        		'password'	=> Hash::make('anakimigrasi')
+        	],
+          [
+        		'fullname'	=> 'Harry Potter',
+        		'email'		=> 'harry@gmail.com',
+        		'password'	=> Hash::make('wingodiumleviosa')
+        	],
+          [
+        		'fullname'	=> 'Albus Dummydor',
+        		'email'		=> 'albus@gmail.com',
+        		'password'	=> Hash::make('youshallnotpass!')
+        	],
+          [
+        		'fullname'	=> 'Sammy Situbondo',
+        		'email'		=> 'samsitubondo@gmail.com',
+        		'password'	=> Hash::make('passwordwuhuuuu')
+        	],
+          [
+        		'fullname'	=> 'Raditya Mika',
+        		'email'		=> 'raditmika@gmail.com',
+        		'password'	=> Hash::make('pagimingguiko')
+        	],
+          [
+        		'fullname'	=> 'Arja Wituna',
+        		'email'		=> 'arjawituna@gmail.com',
+        		'password'	=> Hash::make('sumurlihatjidatsaya!')
+        	],
+          [
+        		'fullname'	=> 'Limbat Idol',
+        		'email'		=> 'limbatidol@gmail.com',
+        		'password'	=> Hash::make('akupendiam')
+        	],
+          [
+        		'fullname'	=> 'Erik Aubameyang',
+        		'email'		=> 'erikau@gmail.com',
+        		'password'	=> Hash::make('erikyolo')
+        	],
+          [
+        		'fullname'	=> 'El Piero',
+        		'email'		=> 'elpiero@gmail.com',
+        		'password'	=> Hash::make('akuanakterbuanghiks')
+        	],
+          [
+        		'fullname'	=> 'Agung Ballotelli',
+        		'email'		=> 'agungganteng@gmail.com',
+        		'password'	=> Hash::make('ototkawat')
+        	],
+          [
+        		'fullname'	=> 'Ibu Kos',
+        		'email'		=> 'ibukos@gmail.com',
+        		'password'	=> Hash::make('koskosan')
+        	],
+          [
+        		'fullname'	=> 'Epan Limas',
+        		'email'		=> 'epan6@gmail.com',
+        		'password'	=> Hash::make('pemainmudaU19')
+        	]
         ];
 
         DB::table('profile_rakyat')->insert($profiles);
