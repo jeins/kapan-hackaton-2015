7e7c512b60ca85ea973875b44a5f169f36e5f514
hilmanrdn <hilmanhgb@gmail.com>
2015-12-06 19:43:50 +0100

    edit komentar

--- KapanFrontend/app/home/controllers/CtrlKomentar.js
+++ KapanFrontend/app/home/controllers/CtrlKomentar.js
@@ -23,12 +23,33 @@ define(['../app.home'], function(app){
             $scope.$emit('totalKomentar', totalKomentar);
         });
 
-        $scope.submitKomentar = function(){console.log(this.komentar_like_toggle);
-            var data = {"post": this.post};
+        $scope.submitKomentar = function(){
+          console.log(this.komentar_like_toggle);
+            var data = {"post": this.post, "like": this.komentar_like_toggle};
             SvcKomentar.addKomentar($stateParams.id, data, function(result){
                 $window.location.reload();
             })
         }
+
+        $scope.submitKomentarPositif = function(){
+
+            var data = {"post": this.post, "like":true};
+            console.log(data);
+            SvcKomentar.addKomentar($stateParams.id, data, function(result){
+                $window.location.reload();
+            })
+        }
+
+        $scope.submitKomentarNegatif = function(){
+
+            var data = {"post": this.post, "like":false};
+            console.log(data);
+            SvcKomentar.addKomentar($stateParams.id, data, function(result){
+                $window.location.reload();
+            })
+        }
+
+
     };
     app.controller(name, dependencies.concat(controller));
 });
