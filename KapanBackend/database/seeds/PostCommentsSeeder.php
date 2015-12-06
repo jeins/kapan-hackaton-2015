4b69023a5c892bbce7b75916ba5e73f53b762d15
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2015-12-06 16:22:23 +0100

    seed comment & post comment

--- KapanBackend/database/seeds/PostCommentsSeeder.php
+++ KapanBackend/database/seeds/PostCommentsSeeder.php
@@ -26,6 +26,56 @@ class PostCommentsSeeder extends Seeder
     			'profile_rakyat_id'	=> 1,
     			'project_posts_id'	=> 3,
     			'comment'			=> 'siap memantau..'
+    		],
+        [
+    			'profile_rakyat_id'	=> 5,
+    			'project_posts_id'	=> 3,
+    			'comment'			=> 'amin...'
+    		],
+        [
+    			'profile_rakyat_id'	=> 7,
+    			'project_posts_id'	=> 6,
+    			'comment'			=> 'Bener bos!'
+    		],
+        [
+    			'profile_rakyat_id'	=> 8,
+    			'project_posts_id'	=> 7,
+    			'comment'			=> 'Kita tunggu aja mas.'
+    		],
+        [
+    			'profile_rakyat_id'	=> 5,
+    			'project_posts_id'	=> 7,
+    			'comment'			=> 'Iya ya. Semoga gak ngambang.'
+    		],
+        [
+    			'profile_rakyat_id'	=> 19,
+    			'project_posts_id'	=> 10,
+    			'comment'			=> 'Keliatannya memang gede banget ya. Harus dipantau terus nih.'
+    		],
+        [
+    			'profile_rakyat_id'	=> 9,
+    			'project_posts_id'	=> 10,
+    			'comment'			=> 'Bener mas *ikut pantau*'
+    		],
+        [
+    			'profile_rakyat_id'	=> 1,
+    			'project_posts_id'	=> 10,
+    			'comment'			=> 'mari sama2 memantau.'
+    		],
+        [
+    			'profile_rakyat_id'	=> 13,
+    			'project_posts_id'	=> 13,
+    			'comment'			=> 'Saya rasa cukup pas sih.'
+    		],
+        [
+    			'profile_rakyat_id'	=> 3,
+    			'project_posts_id'	=> 13,
+    			'comment'			=> 'Lumayan lah. Costnya juga ga terlalu besar.'
+    		],
+        [
+    			'profile_rakyat_id'	=> 7,
+    			'project_posts_id'	=> 13,
+    			'comment'			=> 'Tapi tetap harus dipantau juga.'
     		]
     	];
 
--- KapanBackend/database/seeds/ProjectPostSeeder.php
+++ KapanBackend/database/seeds/ProjectPostSeeder.php
@@ -26,6 +26,61 @@ class ProjectPostSeeder extends Seeder
         		'profile_rakyat_id'	=> 2,
         		'project_info_id'	=> 2,
         		'post'				=> 'Project yang sangat bagus, mari kita pantauu...'
+        	],
+          [
+        		'profile_rakyat_id'	=> 3,
+        		'project_info_id'	=> 3,
+        		'post'				=> 'Semoga terealisasi'
+        	],
+          [
+        		'profile_rakyat_id'	=> 10,
+        		'project_info_id'	=> 4,
+        		'post'				=> 'Mohon kelanjutannya diupdate'
+        	],
+          [
+        		'profile_rakyat_id'	=> 9,
+        		'project_info_id'	=> 5,
+        		'post'				=> 'Proyek yang ditunggu-tunggu. Siap memantau.'
+        	],
+          [
+        		'profile_rakyat_id'	=> 9,
+        		'project_info_id'	=> 6,
+        		'post'				=> 'Kok saya pesimis ya dengen proyek ini.'
+        	],
+          [
+        		'profile_rakyat_id'	=> 11,
+        		'project_info_id'	=> 7,
+        		'post'				=> 'Proses lumayan cepat. Asal jangan asal jadi aja.'
+        	],
+          [
+        		'profile_rakyat_id'	=> 12,
+        		'project_info_id'	=> 7,
+        		'post'				=> 'Semoga sesuai yang diharapkan.'
+        	],
+          [
+        		'profile_rakyat_id'	=> 13,
+        		'project_info_id'	=> 8,
+        		'post'				=> 'Biaya ga terlalu besar nih?'
+        	],
+          [
+        		'profile_rakyat_id'	=> 17,
+        		'project_info_id'	=> 10,
+        		'post'				=> 'Saya sudah menunggu-nunggu proyek ini. Semoga terealisai sesuai yang diharapkan.'
+        	],
+          [
+        		'profile_rakyat_id'	=> 7,
+        		'project_info_id'	=> 10,
+        		'post'				=> 'Mantap. Lanjutkan!'
+        	],
+          [
+        		'profile_rakyat_id'	=> 17,
+        		'project_info_id'	=> 10,
+        		'post'				=> 'Waktu pelaksanaannya terasa agak terlalu lama ya.'
+        	],
+          [
+        		'profile_rakyat_id'	=> 14,
+        		'project_info_id'	=> 9,
+        		'post'				=> 'Updatenya mana ya?'
         	]
         ];
 
